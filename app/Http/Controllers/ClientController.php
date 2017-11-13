b854627ad630fc2fb7bda254abd5653d6ca21d11
root <judeforwork8@gmail.com>
2017-11-14 01:19:00 +0200

    fix edit form

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -75,10 +75,45 @@ $post);
 
     public function update(Request $request, $id)
     {
-        $article = Client::findOrFail($id);
-        $article->update($request->all());
+        
+//$article = Client::findOrFail($id);
+//        $article->update($request->all());
+	$validator = Validator::make($request->all(), [
 
-        return $article;
+            'firstname_id' => 'required',
+
+            'lastname_id' => 'required',
+
+            'email' => 'required|email',
+
+            'adress' => 'required',
+
+        ]);
+
+
+        if ($validator->passes()) {
+//try to submit stored procedure .
+$post = $request->all();
+$post = array_replace(
+array_fill_keys([
+'id',
+'firstname_id',
+'lastname_id',
+'personal_code',
+'email',
+'adress',
+'city_id',
+'country_id',
+'ed',
+'cd',
+],null),$post,['id'=>$id]);
+//var_dump($post); die('new exception!');
+return $data = \DB::select( 'select ClientEdit(:id,:firstname_id,:lastname_id,:personal_code,:email,:adress,:city_id,:country_id,:ed,:cd);',
+$post);
+}
+//
+	return response()->json(['error'=>$validator->errors()->all()]);
+        //return $article;
     }
 
     public function delete(Request $request, $id)
