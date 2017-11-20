442ca9ab6cfa65e838b29b0a33a14262e710eaf0
jude <judeforwork8@gmail.com>
2017-11-20 18:11:48 +0200

    FIX tests

--- tests/Feature/Feature/ClientTest.php
+++ tests/Feature/Feature/ClientTest.php
@@ -21,75 +21,62 @@ class ClientTest extends TestCase
 
     public function testsClientsAreCreatedCorrectly()
     {
-        $user = factory(User::class)->create();
-        $token = $user->generateToken();
+        $user    = factory(User::class)->create();
+        $token   = $user->generateToken();
         $headers = ['Authorization' => "Bearer $token"];
 
         $client = factory(\App\Client::class)->make([
-'personal_code'=>'Mama mila ramu!'
-]);
-	//var_dump($user);die;
-//        $token = $user->generateToken();
-//        $headers = ['Authorization' => "Bearer $token"];
-        //$headers = [];
-//var_dump($user->toArray()); die;
-        ;
+            'personal_code' => 'Mama mila ramu!',
+        ]);
 
         $responce = $this->json('POST', '/api/clients', $client->toArray(), $headers);
-//var_dump($responce->message); die;
-//var_dump($responce->content());die;
-            $responce->assertStatus(201)
-//;
-->assertJson([[
-           'clientinsert' => 51,
-         ]]);
-$this->assertDatabaseHas('clients_view', ['id'=>51]);
-//            ->assertJson(['personal_code' => 'Mama mila ramu!']);
+        $responce->assertStatus(201)
+            ->assertJson([
+                [
+                    'clientinsert' => 51,
+                ],
+            ]);
+        $this->assertDatabaseHas('clients_view', ['id' => 51]);
     }
 
     public function testsClientsAreUpdatedCorrectly()
     {
-        //$user = factory(User::class)->create();
-        //$token = $user->generateToken();
-        //$headers = ['Authorization' => "Bearer $token"];
-    $headers = [];
-        $user = factory(User::class)->create();
-        $token = $user->generateToken();
+        $user    = factory(User::class)->create();
+        $token   = $user->generateToken();
         $headers = ['Authorization' => "Bearer $token"];
 
         $client = factory(\App\Client::class)->create([
             'personal_code' => 'Mama mila ramu!',
-            
+
         ]);
 
         $payload = factory(\App\Client::class)->make(
-[
-'personal_code' => 'Mama_mila_ramu'
-])->toArray();
+            [
+                'personal_code' => 'Mama_mila_ramu',
+            ])->toArray();
 
         $response = $this->json('PUT', '/api/clients/' . $client->id, $payload, $headers)
             ->assertStatus(200)
-            ->assertJson([[ 
-                'clientedit' => 51, 
-        ]]);
-	$this->assertDatabaseHas('clients_view',['id'=>51,
-'personal_code' => 'Mama_mila_ramu',
-]);
+            ->assertJson([
+                [
+                    'clientedit' => 51,
+                ],
+            ]);
+        $this->assertDatabaseHas('clients_view', [
+            'id'            => 51,
+            'personal_code' => 'Mama_mila_ramu',
+        ]);
     }
 
     public function testsClientsAreDeletedCorrectly()
     {
-        // $user = factory(User::class)->create();
-        // $token = $user->generateToken();
-        // $headers = ['Authorization' => "Bearer $token"];
-        $headers = [];
-        $user = factory(User::class)->create();
-        $token = $user->generateToken();
+        $user    = factory(User::class)->create();
+        $token   = $user->generateToken();
         $headers = ['Authorization' => "Bearer $token"];
 
-   $article = factory(\App\Client::class)->create([
+        $article = factory(\App\Client::class)->create([
             'personal_code' => 'Mama mila ramu!',
-            
+
         ]);
 
         $this->json('DELETE', '/api/clients/' . $article->id, [], $headers)
@@ -98,79 +85,58 @@ $this->assertDatabaseHas('clients_view', ['id'=>51]);
 
     public function testClientsAreListedCorrectly()
     {
-        $user = factory(User::class)->create();
-        $token = $user->generateToken();
+        $user    = factory(User::class)->create();
+        $token   = $user->generateToken();
         $headers = ['Authorization' => "Bearer $token"];
 
-   $client = factory(\App\Client::class)->create([
+        $client = factory(\App\Client::class)->create([
             'personal_code' => 'Mama mila ramu! first',
-	    'email' => 'secret@mail.com',
-            
+            'email'         => 'secret@mail.com',
+
         ]);
-//var_dump($article);die;
         $client = factory(\App\Client::class)->create([
             'personal_code' => 'Mama mila ramu! second',
-	    'email' => 'secrets@mail.com'
-            
+            'email'         => 'secrets@mail.com',
+
         ]);
 
-//        $user = factory(User::class)->create();
-  //      $token = $user->generateToken();
-//        $headers = ['Authorization' => "Bearer $token"];
-        //$headers = [];
 
         $response = $this->json('GET', '/api/clients', [], $headers);
         $response
             ->assertJsonFragment(
-    [
-        'personal_code' => 'Mama mila ramu! first',
-        'email' => 'secret@mail.com',
-    ]    );
+                [
+                    'personal_code' => 'Mama mila ramu! first',
+                    'email'         => 'secret@mail.com',
+                ]);
 
         $response->assertJsonFragment(
-    [
-        'personal_code' => 'Mama mila ramu! second',
-        'email' => 'secrets@mail.com',
-    ]    )
+            [
+                'personal_code' => 'Mama mila ramu! second',
+                'email'         => 'secrets@mail.com',
+            ])
             ->assertJsonStructure([
-                '*' => ['id', 'lastname_id','lastname_id','firstname_id','personal_code', 'email','city_id','country_id'],
+                '*' => [
+                    'id',
+                    'lastname_id',
+                    'lastname_id',
+                    'firstname_id',
+                    'personal_code',
+                    'email',
+                    'city_id',
+                    'country_id',
+                ],
             ]);
-//return;
-//$response->
-//assertJson([
-//[
-//        "id"=> 15,
-//        "lastname_id"=> "Altenwerth",
-//        "firstname_id"=> "Hosea",
-//        "personal_code"=> "Qui eos consequatur quia placeat possimus tempore.",
-//        "email"=> "juvenal.friesen@yahoo.com",
-//        "adress"=> "689 Luettgen Isle",
-//        "city_id"=> "Lake Hyman",
-//        "country_id"=> "Norway"
-//    ]
-//]);
-
-//        $response = $this->json('GET', '/api/clients', [], $headers)
-
-//            ->assertStatus(200)
-//            ->assertJson([
-//                [ 'personal_code' => 'Mama mila ramu! first' ],
-//                [ 'personal_code' => 'Mama mila ramu! second' ]
-//            ])
-//            ->assertJsonStructure([
-//                '*' => ['id', 'body', 'title', 'created_at', 'updated_at'],
-//            ]);
     }
 
 
     public function testsGetClientsSuccessfully()
     {
         $payload = [
-            'name' => 'TestName',
-            'email' => 'test@gmail.com',
-            'password' => 'test123',
+            'name'                  => 'TestName',
+            'email'                 => 'test@gmail.com',
+            'password'              => 'test123',
             'password_confirmation' => 'test123',
-	      //'personal_code' => 'Mama mila ramu!'	
+            //'personal_code' => 'Mama mila ramu!'
         ];
 
         $this->json('post', '/api/register', $payload)
@@ -192,22 +158,17 @@ $this->assertDatabaseHas('clients_view', ['id'=>51]);
         $this->json('post', '/api/register')
             ->assertStatus(422)
             ->assertJsonFragment([
-                'name' => ['The name field is required.'],
-                'email' => ['The email field is required.'],
+                'name'     => ['The name field is required.'],
+                'email'    => ['The email field is required.'],
                 'password' => ['The password field is required.'],
             ]);
-    //        ->assertJson([
-   //             'name' => ['The name field is required.'],
-  //              'email' => ['The email field is required.'],
- //               'password' => ['The password field is required.'],
-//            ]);
     }
 
     public function testsRequirePasswordConfirmation()
     {
         $payload = [
-            'name' => 'John',
-            'email' => 'john@toptal.com',
+            'name'     => 'John',
+            'email'    => 'john@toptal.com',
             'password' => 'toptal123',
         ];
 
@@ -218,5 +179,5 @@ $this->assertDatabaseHas('clients_view', ['id'=>51]);
             ]);
     }
 
-    
+
 }
